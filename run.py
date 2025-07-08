2f79073631d8fc7f50936f560418a050a2c97285
Miguel Mayor <mmayor200610@gmail.com>
2025-07-08 19:46:12 -0400

    Primer commit: proyecto simulador emergencia AL

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import app
+import os
 
-if __name__ == "__main__":
-    app.run(host='192.168.1.225', port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
